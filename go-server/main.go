d5b166b75d4e9d57305a6cca7fd963e5ec770c69
Joanne Chow <joannetychow@gmail.com>
2023-02-20 22:02:04 -0800

    Allow uploading image and saving to local folder. Then write path to txt file.

--- go-server/main.go
+++ go-server/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"html/template"
+	"bufio"
 	"log"
 	"net/http"
+	"fmt"
 	"os"
+	"io/ioutil"
 	"regexp"
+	"github.com/google/uuid"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
 	List  []string
+	ImageList []string
 }
 
 func (p *Page) save() error {
@@ -66,6 +71,65 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/edit/"+pageName, http.StatusFound)
 }
 
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	id := uuid.New().String()
+	fmt.Println("File Upload Endpoint Hit")
+
+    // Parse our multipart form, 10 << 20 specifies a maximum
+    // upload of 10 MB files.
+    r.ParseMultipartForm(10 << 20)
+    // FormFile returns the first file for the given key `myFile`
+    // it also returns the FileHeader so we can get the Filename,
+    // the Header and the size of the file
+    file, handler, err := r.FormFile("myFile")
+    if err != nil {
+        fmt.Println("Error Retrieving the File")
+        fmt.Println(err)
+        return
+    }
+
+    defer file.Close()
+    fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+    fmt.Printf("File Size: %+v\n", handler.Size)
+    fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	originalFileName := handler.Filename
+
+    // Create a temporary file within our temp-images directory that follows
+    // a particular naming pattern
+    tempFile, err := ioutil.TempFile("savedImages", "upload-*.png")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer tempFile.Close()
+
+    // read all of the contents of our uploaded file into a
+    // byte array
+    fileBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    // write this byte array to our temporary file
+    tempFile.Write(fileBytes)
+    // return that we have successfully uploaded our file!
+    fmt.Println("Successfully Uploaded File")
+
+    path := tempFile.Name()
+
+    // add file path to txt
+	f, err := os.OpenFile("filepath.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString("id = " + id + "; Original File Name = " + originalFileName + "; File Path = " + path + "\r\n"); err != nil {
+		log.Println(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
 var templates = template.Must(template.ParseFiles("index.html", "edit.html", "view.html"))
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -82,7 +146,24 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		list = append(list, entryName)
     }
 
-	p := &Page{Title: "home", List: list}
+	var imagelist []string
+
+	// TODO: Add DB code to save path
+
+	readfilepath, err := os.Open("filepath.txt")
+	if err == nil {
+		fileScanner := bufio.NewScanner(readfilepath)
+ 
+		fileScanner.Split(bufio.ScanLines)
+	  
+		for fileScanner.Scan() {
+			imagelist = append(imagelist, fileScanner.Text())
+		}
+	} 
+
+	readfilepath.Close()
+
+	p := &Page{Title: "home", List: list, ImageList: imagelist}
 
 	renderTemplate(w, "index", p)
 }
@@ -111,6 +192,7 @@ func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/upload/", uploadHandler)
 	http.HandleFunc("/add/", addHandler)
 	http.HandleFunc("/", getRoot)
 
