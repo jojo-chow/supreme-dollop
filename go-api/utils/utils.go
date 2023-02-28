b3d36cf7530acfe8f6c371c298c342e7f7f32a59
Joanne Chow <joannetychow@gmail.com>
2023-02-27 23:12:30 -0800

    removed typo

--- go-api/utils/utils.go
+++ go-api/utils/utils.go
@@ -4,7 +4,7 @@ import (
     "net/http"
 )
 
-// ReturnJsonResponse function for returning movies data in JSON format
+// ReturnJsonResponse function for returning data in JSON format
 func ReturnJsonResponse(res http.ResponseWriter, httpCode int, resMessage []byte) {
     res.Header().Set("Content-type", "application/json")
     res.WriteHeader(httpCode)
